b3e0d9e812d7236a2a4c179c4ef35efd23fdbcbc
AdnanGhannam2001 <adnanghannam21@gmail.com>
2025-01-01 16:34:04 -0500

    Add the Basics for `Products` Page

--- src/OnlineShop/Controllers/HomeController.cs
+++ src/OnlineShop/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data.Common;
+using OnlineShop.Data.Interfaces;
 using OnlineShop.Models;
 
 namespace OnlineShop.Controllers;
@@ -7,15 +9,22 @@ namespace OnlineShop.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IProductService _productService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,
+        IProductService productService)
     {
         _logger = logger;
+        _productService = productService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index([FromQuery] int pageNumber,
+        [FromQuery] string? categoryId = null)
     {
-        return View();
+        ViewData["categories"] = await _productService.GetCategories();
+        var products = await _productService.GetProducts(new PageRequest(10, pageNumber), categoryId);
+
+        return View(products);
     }
 
     public IActionResult Privacy()

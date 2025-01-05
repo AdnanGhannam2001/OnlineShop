3d2a80af2b9bd413a48924b307d705e4a24b17b9
AdnanGhannam2001 <adnanghannam21@gmail.com>
2025-01-05 16:59:45 -0500

    Add `NotFound` View

--- src/OnlineShop/Controllers/ErrorsController.cs
+++ src/OnlineShop/Controllers/ErrorsController.cs
@@ -6,9 +6,16 @@ namespace OnlineShop.Controllers;
 
 public class ErrorsController : Controller
 {
+    public const string Name = "Errors";
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Index()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult NotFound([FromQuery] string? message)
+    {
+        return View(model: message);
     }
 }
